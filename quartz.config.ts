ecce23866c149b56384b7944dfc1434ee5b6e2dc
unknown <1971644202@qq.com>
2026-04-14 22:03:32 +0800

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "venomyumu.github.io/japan-travel",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
